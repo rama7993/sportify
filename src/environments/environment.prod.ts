96ecb4ea5768d4ca71561a91f31e5ce11880d569
rama7993 <ramakotireddy912@gmail.com>
2025-10-06 22:41:09 +0530

    Fix production environment - use direct values instead of process.env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   spotify: {
-    clientId: process.env['SPOTIFY_CLIENT_ID'],
-    clientSecret: process.env['SPOTIFY_CLIENT_SECRET'],
+    clientId: '5705e43cb6af4b79be9b46229d29fd58',
+    clientSecret: '128c5e60b061432cad4677d49805370a',
   },
   backendApiUrl: 'https://sportify-backend-beta.vercel.app/api/spotify',
 };
