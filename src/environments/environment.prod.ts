574515e53f39de77cb00d6b28268e86c1ce8c482
rama7993 <ramakotireddy912@gmail.com>
2025-10-06 23:04:46 +0530

    privacy check

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
 export const environment = {
   production: true,
   spotify: {
-    clientId:
-      process.env['SPOTIFY_CLIENT_ID'] || '5705e43cb6af4b79be9b46229d29fd58',
-    clientSecret:
-      process.env['SPOTIFY_CLIENT_SECRET'] ||
-      '128c5e60b061432cad4677d49805370a',
+    clientId:'5705e43cb6af4b79be9b46229d29fd58',
+    clientSecret:'128c5e60b061432cad4677d49805370a',
   },
   backendApiUrl: 'https://sportify-backend-beta.vercel.app/api/spotify',
 };
